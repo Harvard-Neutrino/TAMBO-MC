ca81a79edca742c1b24525fe908dcf5452d20d02
Carlos A. Arguelles <carguelles@fas.harvard.edu>
2022-01-03 11:11:42 -0500

    fixed geometry module

--- tambo/src/geometry.jl
+++ tambo/src/geometry.jl
@@ -1,3 +1,10 @@
+module geometry
+
+export Point,
+       Direction,
+       Geometry
+
+# ----------------------------------------------------------------------------
 
 function get_distance_coordinate(latitude, longitude, latmin, longmin)
     latmid = (latitude + latmin)/2.0
@@ -12,25 +19,25 @@ function get_distance_coordinate(latitude, longitude, latmin, longmin)
     x,y
 end
 
-struct Point{T<:Real}
+mutable struct Point{T<:Real}
     longitude::T
     latitude::T
     x::T
     y::T
     z::T
-    function Point(longitude::T, latitude::T, elevation::T; latmin = 0.0, longmin = 0.0) where T<:Real
-    x,y = get_distance_coordinate(latitude,longitude,latmin, longmin)
-    new{T}(longitude, latitude, x, y, elevation)
+    function Point(longitude::T, latitude::T, elevation::T; lat_min = 0.0, long_min = 0.0) where T<:Real
+        x,y = get_distance_coordinate(latitude,longitude,lat_min, long_min)
+        new{T}(longitude, latitude, x, y, elevation)
     end
 end
 
-struct Direction{T<:Real}
+mutable struct Direction{T<:Real}
     phi::T
     theta::T
     x::T
     y::T
     z::T
-    function Direction{T<:Real}(phi::T, theta::T)
+    function Direction(phi::T, theta::T) where T<:Real
         x = cos(theta)*sin(phi)
         y = sin(theta)*sin(phi)
         z = cos(phi)
@@ -45,20 +52,20 @@ using Dierckx
 using DelimitedFiles
 
 struct Geometry
-    lat_min::Float
-    lat_max::Float
-    long_min::Float
-    long_max::Float
-    elev_min::Float
-    elev_max::Float
+    lat_min::Float64
+    lat_max::Float64
+    long_min::Float64
+    long_max::Float64
+    elev_min::Float64
+    elev_max::Float64
     geometry_spline::Spline2D
-    geometry_box
-    density_rock::Float #"kg/m^3"
-    density_air::Float #"kg/m^3" 
-    function Geometry(textfile)
-        data_input, header_input = readdlm("./ColcaValleyData.txt",'\t', '\n', header = true)
-        latitude = deg2rad(data_input[:,2])
-        longitude = deg2rad(data_input[:,3])
+    geometry_box::Vector{Float64}
+    density_rock::Float64 #"kg/m^3"
+    density_air::Float64 #"kg/m^3" 
+    function Geometry(textfile::String)
+        data_input, header_input = readdlm(textfile,'\t', '\n', header = true)
+        latitude = map(deg2rad,data_input[:,2])
+        longitude = map(deg2rad,data_input[:,3])
         elevation = data_input[:,4]
 
         lat_min = minimum(latitude)
@@ -68,7 +75,7 @@ struct Geometry
         elev_min = minimum(elevation)
         elev_max = maximum(elevation)
 
-        number_geo_points = len(latitude)
+        number_geo_points = length(latitude)
 
         # A coord. system in meters with the origin at latmin, latmax
         coordinate_points = ([Point(longitude[i],
@@ -77,7 +84,6 @@ struct Geometry
                                     lat_min, 
                                     long_min) 
         for i in 1:number_geo_points])
-
         geometry_spline = Spline2D(map(p->p.x,coordinate_points),
                                    map(p->p.y,coordinate_points),
                                    map(p->p.z,coordinate_points);
@@ -88,9 +94,16 @@ struct Geometry
         y_max = max(map(p->p.y,coordinate_points))
         geometry_box = [0.0,x_max,0.0,y_max,elev_min,elev_max]
 
-        density_rock =  5520 #"kg/m^3"
+        density_rock = 5520 #"kg/m^3"
         density_air = 1225 #"kg/m^3" 
 
         new(lat_min,lat_max,long_min,long_max,elev_min,elev_max,geometry_spline,geometry_box,density_rock,density_air)
     end
+end
+
+end #module
+
+if abspath(PROGRAM_FILE) == @__FILE__
+    geo = geometry.Geometry("../../resources/ColcaValleyData.txt")
+    println("hola")
 end
