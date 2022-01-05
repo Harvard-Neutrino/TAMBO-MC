a4d3f9dc5a8da2423d66a2421bdee4d88b751e92
jlazar17 <jeff.p.lazar@gmail.com>
2022-01-05 13:53:16 +1100

    Make Track object to governs particle trajectory

--- tambo/src/track.jl
+++ tambo/src/track.jl
@@ -1,22 +1,46 @@
-include("./geometry.jl")
-using .geometry
+module track
 
+    include("./position.jl")
+    using .position
+    export Track, TPoint, is_inside, normalize_track, find_position
 
-function get_start_track(point, dir)
-    #=
-    Builds 6 item array defining starting position and direction of track
-    =#    
-    line_eq = [[point.x, point.y, point.z],
-               [dir.x, dir.y, dir.z]]
-    round.(line_eq, 3)
+    function is_inside(p::TPoint, box::Vector{TPoint})
+        is_it = true
+        for fn in fieldnames(TPoint)
+            edge_1 = getfield(box[1], fn)
+            edge_2 = getfield(box[2], fn)
+            s = sign((edge_2-edge_1))
+            is_it = is_it && (s*edge_1 < s*getfield(p, fn) < s*edge_2)
+            if !is_it
+                return false
+            end
+        end
+        is_it
+    end
+
+    struct Track
+        ipoint::TPoint
+        direction::TPoint
+    end
+
+    function normalize_track(t::Track, box::Vector{TPoint})
+        if !is_inside(t.ipoint, box)
+            error("Track does not start inside the box")
+        end
+        λ_f = Inf
+        for i in 1:length(box)
+            pt      = box[i]
+            prop_λs = (pt - t.ipoint)/t.direction
+            for fn in fieldnames(TPoint)
+                if getfield(prop_λs, fn) > 0
+                    λ_f = minimum((getfield(prop_λs, fn), λ_f))
+                end
+            end
+        end
+        λ_f, Track(t.ipoint, t.direction*λ_f)
+    end
+
+    function find_position(t::Track, λ)
+        t.ipoint+λ*t.direction
+    end
 end
-
-function find_end_points(t::Track, geo::Geometry)
-    
-end
-
-struct Track
-    ipoint::Point
-    direction::Direction
-end
-
