a5a3bcd86ff364d35eff5928215cc718bf668c2a
jlazar17 <jeff.p.lazar@gmail.com>
2022-01-24 12:53:16 -0500

    Move is_inside function to Position.jl

--- tambo/src/Tracks.jl
+++ tambo/src/Tracks.jl
@@ -2,22 +2,8 @@ module Tracks
 
 push!(LOAD_PATH, @__DIR__)
 
-using Position: TPoint
-export Track, is_inside, normalize_track, find_position
-
-function is_inside(p::TPoint, box::Vector{TPoint})
-    is_it = true
-    for fn in fieldnames(TPoint)
-        edge_1 = getfield(box[1], fn)
-        edge_2 = getfield(box[2], fn)
-        s = sign((edge_2-edge_1))
-        is_it = is_it && (s*edge_1 < s*getfield(p, fn) < s*edge_2)
-        if !is_it
-            return false
-        end
-    end
-    is_it
-end
+using Position: TPoint, Box, is_inside
+export Track, normalize_track, find_position
 
 struct Track
     ipoint::TPoint
@@ -31,13 +17,15 @@ struct Track
     end
 end
 
-function normalize_track(t::Track, box::Vector{TPoint})
+# TODO Make this geneeralize to more complex shapes
+function normalize_track(t::Track, box::Box)
     if !is_inside(t.ipoint, box)
         error("Track does not start inside the box")
     end
+    edges = [box.p1, box.p2]
     λ_f = Inf
-    for i in 1:length(box)
-        pt      = box[i]
+    for i in 1:length(edges)
+        pt = edges[i]
         prop_λs = (pt - t.ipoint)/t.direction
         for fn in fieldnames(TPoint)
             if getfield(prop_λs, fn) > 0
