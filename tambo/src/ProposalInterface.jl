1a7e98f6eefafe6102118d29f64e1312f06b87da
jorgegponce <jgarciaponce@boslogin03.rc.fas.harvard.edu>
2022-04-19 02:40:35 -0400

    tested muon ranges

--- tambo/src/ProposalInterface.jl
+++ tambo/src/ProposalInterface.jl
@@ -15,7 +15,7 @@ struct Particle
     energy::Float64
 end
 
-function make_sector(medium::Int64, start::Float64, stop::Float64)
+function make_sector(medium, start, stop)
 
     # Defining a Sector
     sec_def = pp.SectorDefinition()
@@ -23,7 +23,10 @@ function make_sector(medium::Int64, start::Float64, stop::Float64)
     sec_def.geometry = pp.geometry.Sphere(pp.Vector3D(), stop, start)
 
     if medium == 1
-        sec_def.medium = pp.medium.StandardRock()
+        # sec_def.medium = pp.medium.StandardRock()
+
+        # Muon Test only
+        sec_def.medium = pp.medium.Ice()
     else 
         sec_def.medium = pp.medium.Air()
     end
@@ -34,8 +37,8 @@ function make_sector(medium::Int64, start::Float64, stop::Float64)
     # The way it was
     sec_def.crosssection_defs.brems_def.lpm_effect = true
     sec_def.crosssection_defs.epair_def.lpm_effect = true
-    sec_def.cut_settings.ecut = 0
-    sec_def.cut_settings.vcut = 0.05
+    sec_def.cut_settings.ecut = -1.0
+    sec_def.cut_settings.vcut = 1e-3
     # sec_def.crosssection_defs.brems_def.lpm_effect = false
     # sec_def.crosssection_defs.epair_def.lpm_effect = false
     # sec_def.cut_settings.ecut = 500
@@ -43,6 +46,7 @@ function make_sector(medium::Int64, start::Float64, stop::Float64)
 
     # Again, not sure what this means
     sec_def.do_continuous_randomization = true
+    sec_def.crosssection_defs.photo_def.parametrization = pp.parametrization.photonuclear.PhotoParametrization.AbramowiczLevinLevyMaor97
 
     return sec_def
 
@@ -50,14 +54,17 @@ end
 
 # Make this useful
 function define_particle(particle::Particle)
+    # {16: 'TauMinusDef', -16: 'TauPlusDef',
+    #          14: 'MuMinusDef',  -14: 'MuPlusDef',
+    #          12: 'EMinusDef',   -12: 'EPlusDef',}
     return pp.particle.MuMinusDef()
 end
 
 
-function make_medium(medium::Vector{Tuple{Int64,Float64}})
+function make_medium(medium)
 
     # Definig sectors from medium (Array) and calculating detector length
-    sectors = Vector{PyObject}()
+    sectors = Vector()
 
     println("Defining Sectors:")
 
@@ -86,7 +93,7 @@ function make_medium(medium::Vector{Tuple{Int64,Float64}})
 end
 
 
-function make_propagator(particle::Particle, medium::Vector{Tuple{Int64,Float64}})
+function make_propagator(particle::Particle, medium)
 
     sectors, detector_length = make_medium(medium)
 
@@ -109,16 +116,20 @@ function make_propagator(particle::Particle, medium::Vector{Tuple{Int64,Float64}
 end
 
 
-function propagate_mcp(particle::Particle, medium::Vector{Tuple{Int64,Float64}}, mcp_energies::Vector{Float64},)
+function propagate_mcp(particle::Particle, energy::Float64, iterations::Int64, medium = nothing, propagator = nothing)
 
     # medium = [(1,0.5e19),(0,0.5e19),(1,0.928e19),(0,0.01289e19),(0,0.1e19)]
 
     # particle = Particle(1.0,1.0)
 
     particle_def = define_particle(particle)
+    if propagator == nothing
+        prop = make_propagator(particle,medium)
+    else
+        prop = propagator
+    end
 
-    prop = make_propagator(particle,medium)
-
+    # Probably gonna have to fix this.
     mu = pp.particle.DynamicData(particle_def.particle_type)
     mu.position = pp.Vector3D(0, 0, 0)
     mu.direction = pp.Vector3D(0, 0, -1)
@@ -137,16 +148,17 @@ function propagate_mcp(particle::Particle, medium::Vector{Tuple{Int64,Float64}},
     photo = Vector{Vector}()
 
     # pp.RandomGenerator.get().set_seed(1234)
+    mu.energy = energy
 
     println("\n\nSTARTING PROPAGATION\n\n")
 
-    for e in tqdm(mcp_energies)
+    for i in tqdm(1:1:iterations)
 
         # secondaries = prop.propagate(mu).particles
         # push!(mu_length, secondaries[end].position.magnitude()/100.0)
         # push!(n_secondaries, size(secondaries, 1))
 
-        mu.energy = e
+        #mu.energy = e
 
         secondaries = prop.propagate(mu)
 
@@ -196,16 +208,40 @@ end
 end # module
 
 using .ProposalInterface
+using Statistics
 
+particle = Particle(1.0,1.0)
+medium = [(1,1e6)]
+energy = collect(6:0.33:11)
+prop = make_propagator(particle,medium)
 
-medium = [(1,0.5e5),(0,0.5e5),(1,0.928e5),(0,0.01289e5),(0,0.1e5)]
+energies = Vector{Float64}()
+range = Vector{Float64}()
+error = Vector{Float64}()
 
-particle = particle = Particle(1.0,1.0)
+for e in energy
+    println("\n\n\nLogEnergy: $e MeV")
+    mu_length, n_secondaries, E = ProposalInterface.propagate_mcp(particle, 10^e, 500, medium, prop)
+    d = mean(mu_length)
+    err = std(mu_length)
+    println("Mean Range: $d")
+    println("Standard Deviation: $err")
+    push!(range, d)
+    push!(energies, e)
+    push!(error, err)
+end
 
-mcp_energies = [1e1,1e2, 1e3, 1e4, 1e5, 1e6, 1e7, 1e8, 1e9, 1e10]
+println(error)
+println(range)
+println(energies)
 
-mu_length, n_secondaries, E = propagate_mcp(particle, medium, mcp_energies)
+# writedlm( "Results/MuMinus_Length.csv",  mu_length, ',')
+# writedlm( "Results/MuMinus_Secondaries.csv",  n_secondaries, ',')
 
-print(mu_length)
-print(n_secondaries)
-print(E)
+# println("Files saved in Results Folder")
+
+# mu_length, n_secondaries, E = propagate_mcp(particle, medium, mcp_energies)
+
+# print(mu_length)
+# print(n_secondaries)
+# print(E)
