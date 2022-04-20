361c987785ad17a0a4e8c5d2324167b241f288d4
jlazar17 <jeff.p.lazar@gmail.com>
2022-04-20 11:31:38 -0400

    Add constructors

--- tambo/src/Particles.jl
+++ tambo/src/Particles.jl
@@ -1,8 +1,20 @@
 module Particles
 
+using PowerLaws: PowerLaw, sample
+
 struct Particle
     pdg_id::Int
     energy::Float64
+    function Particle(pdg_id::Int, inv_cdf::Function)
+        u = rand()
+        e = inv_cdf(u)
+        new(pdg_id, e)
+    end
+    function Particle(pdg_id::Int, pl::PowerLaw)
+        # TODO this is sloppy
+        e = sample(pl)[1]
+        new(pdg_id, e)
+    end
 end
 
 function interact(p::Particle)
