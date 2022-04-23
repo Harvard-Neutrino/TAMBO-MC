da04daf7c21d2c6f5c078fcd710896d5dd64c3dd
jlazar17 <jeff.p.lazar@gmail.com>
2022-04-23 10:12:20 -0400

    Add column depth an inverse column depth functions

--- tambo/src/Tracks.jl
+++ tambo/src/Tracks.jl
@@ -3,9 +3,11 @@ module Tracks
 push!(LOAD_PATH, @__DIR__)
 
 using Geometry: TPoint, Box, is_inside
+using Units
+using Unitful
 using LinearAlgebra
-using Roots: find_zeros
-export Track, Direction, total_column_depth
+using Roots: find_zeros, find_zero
+export Track, Direction, total_column_depth, column_depth, inverse_column_depth
 
 struct Direction
     θ
@@ -67,7 +69,7 @@ function intersect_box(p::TPoint, d::Direction, box::Box)
     λ is proportional to the distance the particle propagates
     We will use distance interchangably with λ
     =#
-    λ_f = Inf
+    λ_f = Inf*m
     # Iterate over points which define box
     for edge in edges
         # Points where track shares coordinate with box edges
@@ -81,7 +83,7 @@ function intersect_box(p::TPoint, d::Direction, box::Box)
             λ = 0 means the tracks starts on an edge of the box
             We want a forward-going track
             =#
-            if λ >= 0
+            if λ >= 0m
                 #=
                 The track hits the box at minimum prop distance
                 Else it will be outside the box before travelling that distance
@@ -101,18 +103,24 @@ function reduce_f(t::Track, f)
     g(λ) = f(t(λ).x, t(λ).y)
 end
 
-function total_column_depth(t::Track, valley; ρ_air=1.225e-3, ρ_rock=2.6)
+function inverse_column_depth(tr::Track, cd, valley)
+    f(λ) = (column_depth(tr, λ, valley) - cd).val
+    λ_int = find_zero(f, (0,1))
+    println(λ_int)
+    λ_int
+end
+
+function column_depth(t, Λ, valley; ρ_air=ρ_air0, ρ_rock=ρ_rock0)
+#function column_depth(t::Track, Λ, valley; ρ_air=ρ_air0, ρ_rock=ρ_rock0)
     oned_valley = reduce_f(t, valley)
-    println(t(0.5).z)
-    println(oned_valley(0.5))
     root_func(λ) = oned_valley(λ)-t(λ).z
-    println(root_func(0.5))
-    zeros = find_zeros(root_func, 0, 1)
-    rgen = vcat([0], zeros, [1])
-    ranges = [(x[2]-x[1], is_inside(t((x[1]+x[2])/2), valley))
-              for x in zip(rgen[1:end-1], rgen[2:end])
-             ]
-    cd = 0
+    zeros = find_zeros(root_func, 0, Λ)
+    rgen = vcat([0], zeros, [Λ])
+    ranges = [
+        (x[2]-x[1], is_inside(t((x[1]+x[2])/2), valley))
+        for x in zip(rgen[1:end-1], rgen[2:end])
+    ]
+    cd = 0mwe
     for x in ranges
         width, in_mountain = x
         ρ = in_mountain ? ρ_rock : ρ_air
@@ -120,5 +128,26 @@ function total_column_depth(t::Track, valley; ρ_air=1.225e-3, ρ_rock=2.6)
     end
     cd
 end
+
+#function total_column_depth(t::Track, valley; ρ_air=1.225e-3, ρ_rock=2.6)
+function total_column_depth(t, valley; ρ_air=1.225e-3, ρ_rock=2.6)
+    column_depth(t, 1, valley)
+end
+#function total_column_depth(t::Track, valley; ρ_air=1.225e-3, ρ_rock=2.6)
+#    oned_valley = reduce_f(t, valley)
+#    root_func(λ) = oned_valley(λ)-t(λ).z
+#    zeros = find_zeros(root_func, 0, 1)
+#    rgen = vcat([0], zeros, [1])
+#    ranges = [(x[2]-x[1], is_inside(t((x[1]+x[2])/2), valley))
+#              for x in zip(rgen[1:end-1], rgen[2:end])
+#             ]
+#    cd = 0
+#    for x in ranges
+#        width, in_mountain = x
+#        ρ = in_mountain ? ρ_rock : ρ_air
+#        cd += width*t.norm*ρ
+#    end
+#    cd
+#end
 
 end # module
