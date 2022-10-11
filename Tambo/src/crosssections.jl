b7e0555a1b2f056bac0c872969553b5233c7d20a
Jeffrey Lazar <jeff.p.lazar@gmail.com>
2022-10-11 09:12:08 -0400

    Reformat

--- Tambo/src/crosssections.jl
+++ Tambo/src/crosssections.jl
@@ -2,7 +2,7 @@ using HDF5
 using StatsBase
 using GridInterpolations
 
-const pdg_to_str = Dict{Int, String}(12 => "nue", 14 => "numu", 16 => "nutau")
+const pdg_to_str = Dict{Int,String}(12 => "nue", 14 => "numu", 16 => "nutau")
 
 struct DifferentialXS
     # Discuss if we want to allow for there not to be only one type of nucleon
@@ -12,54 +12,52 @@ struct DifferentialXS
     emin::Float64
     xs::Matrix{Float64}
     function DifferentialXS(zs, a, es, xs)
-        size(xs)[1]==size(es)[1] || error("We need a cross section for each energy")
-        size(xs)[2]==size(zs)[1] || error("Number of zs wrong")
-        size(xs)[2]==size(a)[1] || error("holder array is not the right size")
-        zs[1]==0 && zs[end]==1 || error("zs need to go from 0 to 1")
+        size(xs)[1] == size(es)[1] || error("We need a cross section for each energy")
+        size(xs)[2] == size(zs)[1] || error("Number of zs wrong")
+        size(xs)[2] == size(a)[1] || error("holder array is not the right size")
+        zs[1] == 0 && zs[end] == 1 || error("zs need to go from 0 to 1")
         zs = LinRange(0, 1, length(a))
         emin = 1e10
-        new(zs, a, es, emin, xs)
+        return new(zs, a, es, emin, xs)
     end
 end
 
 function DifferentialXS(fname::String, ν_pdg::Int, interaction::String)
     h5f = h5open(fname)
-    es = h5f["energies"][:]
+    es = 10 .^ h5f["energies"][:]
     zs = h5f["zs"][:]
-    flavor  = pdg_to_str[abs(ν_pdg)]
-    ν_pdg > 0 ? nt = "nu" : nt = "nubar"
-    xs = 10 .^ (h5f[flavor][nt][interaction][:,:])
+    flavor = pdg_to_str[abs(ν_pdg)]
+    nt = ν_pdg > 0 ? "nu" : "nubar"
+    xs = h5f["$(flavor)_$(nt)_$(interaction)"][:, :]
     a = zeros(length(zs))
-    DifferentialXS(zs, a, es, xs)
+    close(h5f)
+    return DifferentialXS(zs, a, es, xs)
 end
 
 function DifferentialXS()
     fname = "$(@__DIR__)/../../../resources/cross_sections/tables/csms_differential.h5"
     ν_pdg = 16
     interaction = "CC"
-    DifferentialXS(fname, ν_pdg, interaction)
+    return DifferentialXS(fname, ν_pdg, interaction)
 end
 
 function DifferentialXS(fname::String)
     ν_pdg = 16
     interaction = "CC"
-    DifferentialXS(fname, ν_pdg, interaction)
+    return DifferentialXS(fname, ν_pdg, interaction)
 end
 
 function DifferentialXS(fname::String, interaction::String)
     ν_pdg = 16
-    DifferentialXS(fname, ν_pdg, interaction)
+    return DifferentialXS(fname, ν_pdg, interaction)
 end
 
 function DifferentialXS(fname::String, ν_pdg::Int)
     interaction = "CC"
-    DifferentialXS(fname, ν_pdg, interaction)
+    return DifferentialXS(fname, ν_pdg, interaction)
 end
 
-
-struct TotalXS
-
-end
+struct TotalXS end
 
 """
     searchsortednearest(a, x)
@@ -78,13 +76,19 @@ julia> searchsortednearest([1, 3, 5, 10], 4)
 """
 function searchsortednearest(a, x)
     idx = searchsortedfirst(a, x)
-    if (idx==1); return idx; end
-    if (idx>length(a)); return length(a); end
-    if (a[idx]==x); return idx; end
-    if (abs(a[idx]-x) < abs(a[idx-1]-x))
+    if (idx == 1)
+        return idx
+    end
+    if (idx > length(a))
+        return length(a)
+    end
+    if (a[idx] == x)
+        return idx
+    end
+    if (abs(a[idx] - x) < abs(a[idx - 1] - x))
         return idx
     else
-        return idx-1
+        return idx - 1
     end
 end
 
@@ -107,12 +111,12 @@ end
 
 Draw samples from 
 """
-function StatsBase.sample(n::Int, xs::DifferentialXS, e::T) where T<:Number
+function StatsBase.sample(n::Int, xs::DifferentialXS, e::T) where {T<:Number}
     get_xs!(xs.a, e, xs.es, xs.xs)
     x = sample(xs.zs, Weights(xs.a), n)
-    x .* (e-xs.emin) .+ xs.emin
+    return x .* (e - xs.emin) .+ xs.emin
 end
 
-function StatsBase.sample(xs::DifferentialXS, e::T) where T<:Number
-    sample(1, xs, e)[1]
+function StatsBase.sample(xs::DifferentialXS, e::T) where {T<:Number}
+    return sample(1, xs, e)[1]
 end
