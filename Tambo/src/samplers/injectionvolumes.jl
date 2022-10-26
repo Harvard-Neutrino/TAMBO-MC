b801e4d82b70ab7ab78b31905435239f1e54ff72
Jeffrey Lazar <jeff.p.lazar@gmail.com>
2022-10-26 16:13:25 -0400

    .

--- Tambo/src/samplers/injectionvolumes.jl
+++ Tambo/src/samplers/injectionvolumes.jl
@@ -1,48 +1,86 @@
 using StaticArrays: SVector
 
-struct InjectionVolume
+struct SymmetricInjectionCylinder
     r_injection::Float64
     l_endcap::Float64
 end
 
-function Base.show(io::IO, volume::InjectionVolume)
+function Base.show(io::IO, cylinder::SymmetricInjectionCylinder)
     print(
         io,
         """
-        r_injection (m): $(volume.r_injection / units.m)
-        l_endcap (m): $(volume.l_endcap / units.m)
-        """,
+        r_injection (m): $(cylinder.r_injection / units.m)
+        l_endcap (m): $(cylinder.l_endcap / units.m)
+        """
+    )
+end
+
+struct AsymmetricInjectionCylinder
+    r_injection::Float64
+    l_incoming_endcap::Float64
+    l_outgoing_endcap::Float64
+end
+
+function Base.show(io::IO, cylinder::AsymmetricInjectionCylinder)
+    print(
+        io,
+        """
+        r_injection (m): $(cylinder.r_injection / units.m)
+        l_incoming_endcap (m): $(cylinder.l_incoming_endcap / units.m)
+        l_outgoing_endcap (m): $(cylinder.l_outgoing_endcap / units.m)
+        """
     )
 end
 
 """
-    Base.rand(volume::InjectionVolume)
+    Base.rand(cylinder::SymmetricInjectionCylinder)
 
 Sample an point of closest approach uniformly on a circle lying
 in the xy-plane
 
 # Example
 ```julia-repl
-julia> injector = InjectionVolume(900units.m, 1units.km);
+julia> injector = SymmetricInjectionCylinder(900units.m, 1units.km);
 julia> rand.seed!(0);
 julia> rand(injector)
 TBW
 ```
 """
-function Base.rand(volume::InjectionVolume)
-    # Sample impact parameter uniformly on a disc
-    b = volume.r_injection .* sqrt(rand())
-    # Sample angle on disc 
+function Base.rand(cylinder::SymmetricInjectionCylinder)
+    b = cylinder.r_injection .* sqrt(rand())
     ψ = rand(Uniform(0, 2π))
     p = SVector{3}([b * cos(ψ), b * sin(ψ), 0])
     return p
 end
 
 """
-    (volume::InjectionVolume)(p::SVector{3})
+    Base.rand(cylinder::AsymmetricInjectionCylinder)
+
+Sample an point of closest approach uniformly on a circle of
+radius r = cylinder.r_injection lying in the xy-plane. This can then
+be rotated to a plane perpendicular to a particle's direction to
+give the point of closest approach
+
+# Example
+```julia-repl
+julia> injector = AsymmetricInjectionCylinder(900units.m, 1units.km);
+julia> rand.seed!(0);
+julia> rand(injector)
+TBW
+```
+"""
+function Base.rand(cylinder::AsymmetricInjectionCylinder)
+    b = cylinder.r_injection .* sqrt(rand())
+    ψ = rand(Uniform(0, 2π))
+    p = SVector{3}([b * cos(ψ), b * sin(ψ), 0])
+    return p
+end
+
+"""
+    (cylinder::Injectioncylinder)(p::SVector{3})
 
 TBW
 """
-function (volume::InjectionVolume)(p::SVector{3})
-    return 1 / (π * volume.r_injection^2)
+function (cylinder::SymmetricInjectionCylinder)(p::SVector{3})
+    return 1 / (π * cylinder.r_injection^2)
 end
