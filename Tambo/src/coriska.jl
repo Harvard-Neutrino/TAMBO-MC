9c07e0446043c914341e3c6a961480857620bc7b
Jeffrey Lazar <jeff.p.lazar@gmail.com>
2022-10-28 18:30:15 -0400

    Add corsika.jl

---
+++ Tambo/src/coriska.jl
@@ -0,0 +1,7 @@
+"""
+CORSIKA orgin is at intersection of particle and observation plane
+x-axis is aligned north
+y-axis is aligned west
+Look at page 97 of the CORISKA manual for this information
+"""
+
