ca24535e0e29bfc1f63210e4594be1f963c583e3
Jeffrey Lazar <jeff.p.lazar@gmail.com>
2024-04-28 19:47:03 +0200

    Fix bug

--- scripts/trigger_defs.jl
+++ scripts/trigger_defs.jl
@@ -12,18 +12,25 @@ function did_trigger(hit_map, module_trigger_thresh=3, event_trigger_thresh=30)
     if length(hit_map) < 3
         return false
     end
-    hit_map = filter(kv->length(kv[2]) >= module_trigger_thresh, hit_map)
-    if length(hit_map) < 3
-        return false
-    end
+    hit_map = filter(kv->sum(corsika_int_weight(kv[2])) >= module_trigger_thresh, hit_map)
     nhit = get_nhit(hit_map)
     return nhit >= event_trigger_thresh
 end
 
-function corsika_int_weight(event::Tambo.CorsikaEvent)
+function corsika_int_weight(event::Tambo.CorsikaEvent) :: Int
+    Random.seed!(4)
     weight = event.weight
     if weight > 1
         weight = rand(Poisson(weight))
     end
     return weight
+end
+
+function corsika_int_weight(event::Tambo.CorsikaEvent, seed::Int) :: Int
+    Random.seed!(seed)
+    return corsika_int_weight(event)
+end
+
+function corsika_int_weight(events::Vector{Tambo.CorsikaEvent}) :: Vector{Int}
+    return [corsika_int_weight(event) for event in events]
 end
