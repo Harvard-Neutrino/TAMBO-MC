9b26d2ebec21a90c92ad5f081b4f2ae3250baad3
thomwg11 <will_thompson@g.harvard.edu>
2024-10-24 10:59:57 -0400

    Initial sim with perfect valley

--- scripts/1_make_event_dicts/make_event_dicts_parallel.jl
+++ scripts/1_make_event_dicts/make_event_dicts_parallel.jl
@@ -1,5 +1,5 @@
 using Pkg
-Pkg.activate("../Tambo")
+Pkg.activate("../../Tambo")
 using Tambo
 using PyCall
 using JLD2
@@ -8,27 +8,27 @@ using ArgParse
 using Glob
 using Distributions
 using LinearAlgebra
+using Parquet2
+using DataFrames 
+using ProgressBars
 
-const pq = PyNULL()
-const np = PyNULL()
-copy!(pq, pyimport("pyarrow.parquet"))
-copy!(np, pyimport("numpy"))
+#const zcorsika = whitepaper_normal_vec.proj
+#as defined in corsika 
+#const xcorsika = SVector{3}([0,-zcorsika[3]/sqrt(zcorsika[2]^2+zcorsika[3]^2),zcorsika[2]/sqrt(zcorsika[2]^2+zcorsika[3]^2)])
+#const ycorsika = cross(zcorsika,xcorsika)
 
-const ycorsika = SVector{3}([0.89192975455881607, 0.18563051261662877, -0.41231374670066206])
-const xcorsika = SVector{3}([0, -0.91184756344828699, -0.41052895273466672])
-const zcorsika = whitepaper_normal_vec.proj
-const xyzcorsika = inv([
-    xcorsika.x xcorsika.y xcorsika.z;
-    ycorsika.x ycorsika.y ycorsika.z;
-    zcorsika.x zcorsika.y zcorsika.z;
-])
-const sim = jldopen("/n/holylfs05/LABS/arguelles_delgado_lab/Lab/common_software/source/corsika8/corsika-work/WhitePaper_300k.jld2")
-const config = SimulationConfig(; Dict(k=>v for (k, v) in sim["config"] if k != :geo_spline_path)...)
-const geo = Tambo.Geometry(config)
-const plane = Tambo.Plane(whitepaper_normal_vec, whitepaper_coord, geo)
+#const xyzcorsika = inv([
+#    xcorsika.x xcorsika.y xcorsika.z;
+#    ycorsika.x ycorsika.y ycorsika.z;
+#    zcorsika.x zcorsika.y zcorsika.z;
+#])
 
-const altmin = 1.8925255158436627units.km
-const altmax = 4.092525515843662units.km
+#const sim = jldopen("/n/holylfs05/LABS/arguelles_delgado_lab/Lab/common_software/source/corsika8/corsika-work/WhitePaper_300k.jld2")
+#const config = SimulationConfig(; Dict(k=>v for (k, v) in sim["config"] if k != :geo_spline_path)...)
+#const geo = Tambo.Geometry("/Users/pavelzhelnin/Documents/physics/TAMBO/resources/tambo_spline.jld2", Tambo.whitepaper_coord)
+#const plane = Tambo.Plane(whitepaper_normal_vec, whitepaper_coord, geo)
+#const altmin = 1.8925255158436627units.km
+#const altmax = 4.092525515843662units.km
 
 include("utils.jl")
 
@@ -38,7 +38,13 @@ function parse_commandline()
         "--basedir"
             help = "Directory with all the simulation directories inside"
             arg_type = String
-            required = true
+            #required = true
+            default = "/Users/pavelzhelnin/Documents/physics/TAMBO/resources/airshowers/GraphNet_00000_00001"
+        "--simfile"
+            help = "Simulation file, if no simulation file produced you need to specify plane coord, vector and spline path"
+            arg_type = String
+            #required = true 
+            default = "/Users/pavelzhelnin/Documents/physics/TAMBO/resources/example_sim_file.jld2"
         "--outfile"
             help = "where to store the output"
             arg_type = String
@@ -63,35 +69,69 @@ function parse_commandline()
             help = "A description that will become the group name for the file"
             arg_type = String
             default = ""
+        "--altmin"
+            help = "Minimum altitude in m"
+            arg_type = Float64
+            default = 1892.5255158436627
+        "--altmax"
+            help = "Maximum altitude in m"
+            arg_type = Float64
+            default = 4092.525515843662 
+       
     end
     return parse_args(s)
 end
 
-function add_hits!(d::Dict, events, modules)
+function add_hits!(d::Dict, og_df::DataFrame, modules)
     rmax = maximum([norm(m.extent) for m in modules])
-    for (idx, event) in enumerate(events)
-        a = inside.(Ref(event.pos), modules, rmax)
-        s = sum(a)
-        @assert s <= 1 "Seems like you're in more than one module.. That doesn't seem right"
-        if s > 0
-            mod = modules[findfirst(a)]
-            if !(mod.idx in keys(d))
-                d[mod.idx] = Tambo.CorsikaEvent[]
-            end
-            push!(d[mod.idx], event)
+    for m in modules
+        df = copy(og_df)
+       
+        df.x .-= m.pos[1]
+        df.y .-= m.pos[2]
+        df.z .-= m.pos[3]
+
+        filter!([:x,:y,:z] => (x,y,z) -> abs(x) <= rmax && abs(y) <= rmax && abs(z) <=rmax, df)
+        
+        if isempty(df)
+            continue 
         end
-    end
-end
+
+        for particle in eachrow(df)
+            particle[[:x,:y,:z]] = m.rot * Vector(particle[[:x,:y,:z]])
+        end 
+
+        filter!([:x,:y,:z] => (x,y,z) -> all(abs.([x,y,z]) .< m.extent / 2), df)
+
+        if isempty(df)
+            continue 
+        else 
+            if !(m.idx in keys(d))
+                d[m.idx] = Tambo.CorsikaEvent[]
+            end
+            for particle in eachrow(df)
+                position = SVector{3}([particle.x,particle.y,particle.z])
+                push!(d[m.idx], Tambo.CorsikaEvent(particle.pdg,
+                particle.kinetic_energy,position,particle.time,particle.weight))
+            end 
+        end 
+    end 
+end 
+
 
 function make_hit_map(
     event_number,
     modules,
-    basedir;
+    basedir,
+    xyzcorsika;
     xmax=nothing,
     xmin=nothing,
     ymin=nothing,
     ymax=nothing
 )
+
+    println("making hit map for event $event_number")
+   
     if isnothing(xmin)
         xmin = minimum([m.pos.x for m in modules])
     end
@@ -107,30 +147,34 @@ function make_hit_map(
 
     d = Dict{Int, Vector{Tambo.CorsikaEvent}}()
     files = find_extant_files(event_number, basedir)
+    #for file in tqdm(files)
     for file in files
-        # This nastiness needs to be here cuz sometimes the file is empty(?)
-        # and it will error in an annoying way. I think the `pqf = nothing` is superfluous
-        # but I put it there before and I'm worried it will cause issues
-        pqf = nothing
-        #try
-            pqf = pq.ParquetFile(file)
-        #catch
-        #    continue
-        #end
-        jdx = 1
-        for batch in pqf.iter_batches()
-            events = loadcorsika(batch)
-            jdx += 1
-            events = filter(
-                e -> xmin < e.pos.x && e.pos.x < xmax && ymin < e.pos.y && e.pos.y < ymax,
-                events
-            )
-            add_hits!(d, events, modules)
-            # These two lines avoid runaway RAM usage
-            events = nothing
-            GC.gc()
+        #CORSIKA8 sometimes doesn't finish b/c job times out 
+        try
+            Parquet2.Dataset(file)
+        catch
+            println("This file can't be read (maybe job timed out): $file")
+            continue
         end
-    end
+
+        pqf = Parquet2.Dataset(file)
+        df = DataFrame(pqf)
+        df = df = loadcorsika(select(df,Not("shower","nx","ny","nz")),xyzcorsika)
+        println("xmin: $(xmin/units.km), xmax: $(xmax/units.km), ymin: $(ymin/units.km), ymax: $(ymax/units.km)")
+        println("event x mean: $(mean(df.x)/units.km)")
+        println("event y mean: $(mean(df.y)/units.km)")
+        df = filter(
+                e -> xmin < e.x && e.x < xmax && ymin < e.y && e.y < ymax,
+                df
+            )
+        add_hits!(d, df, modules)
+      
+
+        #if necessary to avoid runaway RAM usage
+        #GC.gc()
+        #close(pqf)
+    end 
+    
     return d
 end
 
@@ -148,10 +192,27 @@ end
 
 function main()
     args = parse_commandline()
-    
     @assert args["njob"] <= args["nparallel"]
+
+    #should fix so that we have a .toml 
+    sim = jldopen(args["simfile"])
+    config = SimulationConfig(; Dict(k=>v for (k, v) in sim["config"])...)
+    geo = Tambo.Geometry(config)
+    plane = Tambo.Plane(config.plane_orientation, config.tambo_coordinates, geo)
+    altmin = args["altmin"]units.m
+    altmax = args["altmax"]units.m
+    
+    zcorsika = config.plane_orientation.proj
+    xcorsika = SVector{3}([0,-zcorsika[3]/sqrt(zcorsika[2]^2+zcorsika[3]^2),zcorsika[2]/sqrt(zcorsika[2]^2+zcorsika[3]^2)])
+    ycorsika = cross(zcorsika,xcorsika)
+    xyzcorsika = inv([
+        xcorsika.x xcorsika.y xcorsika.z;
+        ycorsika.x ycorsika.y ycorsika.z;
+        zcorsika.x zcorsika.y zcorsika.z;
+    ])
+
     modules = Tambo.make_detector_array(
-        whitepaper_coord,
+        Tambo.perfect_valley_coord,
         args["length"]units.m,
         args["deltas"]units.m,
         altmin,
@@ -159,6 +220,7 @@ function main()
         plane,
         geo
     )
+
     outfile = args["outfile"]
     if args["nparallel"] > 1
         outfile = replace(outfile, ".jld2"=>"_$(args["njob"])_$(args["nparallel"]).jld2")
@@ -175,16 +237,23 @@ function main()
     end
 
     event_numbers = get_event_numbers(args["basedir"])[args["njob"]:args["nparallel"]:end]
-    for event_number in event_numbers
+    println("creating event dicts...")
+    println("event numbers: $event_numbers")
 
+    #for (_,event_number) in tqdm(enumerate(event_numbers))
+    for (_, event_number) in enumerate(event_numbers)
+        println("This is event number: $event_number")
         if can_skip_event(event_number, outfile, run_desc)
+            println("skipped event $event_number")
             continue
         end
-        hit_map = make_hit_map(event_number, modules, args["basedir"])
+        println("event number $event_number is not skipped")
+        hit_map = make_hit_map(event_number, modules, args["basedir"],xyzcorsika)
 
         jldopen(outfile, "r+") do jldf
             jldf["$(run_desc)/$(event_number)"] = hit_map
         end
+
     end
 end
 
